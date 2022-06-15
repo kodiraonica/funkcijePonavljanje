1debb2a0d8a5f01aea6c28c4d66ca68bb2f922bf
Alex <aleksandar.hren2@gmail.com>
2022-06-15 19:26:03 +0200

    Gotov zadatak

--- js/app.js
+++ js/app.js
@@ -10,4 +10,23 @@
     setInterval(() => {}, 1000) --> kreira ponavljajući interval koji se događa svaku sekundu
     clearInterval(myInterval) --> poništava interval kada nam više ne treba
 */
-function startCount() {}
+let counter = document.getElementById("printCounter");
+let brojac = 0;
+
+function ispisBroja(a) {
+  setInterval(() => {
+    if (brojac < a) {
+      brojac++;
+      counter.innerHTML = brojac;
+      console.log(brojac);
+    } else {
+      clearInterval(ispisBroja);
+    }
+  }, 1000);
+}
+
+function startCount() {
+  let input = document.getElementById("inputCounter").value;
+  brojac = 0;
+  ispisBroja(input);
+}
