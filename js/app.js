64cb6c5efc9ed40fbbf839a1f628366861482965
Denis Martin Budinski <denis.budinski@endcoding.hr>
2022-06-15 15:56:47 +0200

    pocetno stanje projekta

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/*
+    ZADATAK
+    Dohvatiti vrijednost koja se unese u input element i napraviti funkciju koja će povećavati
+    i prikazivati vrijednost onoliko puta koliko je bilo uneseno u input elementu
+ */
+/* 
+    help
+    document.getElementById("inputCounter").value --> dohvaća vrijednost elementa
+    document.getElementById("printCounter") --> dohvaća element koji ćemo puniti
+    setInterval(() => {}, 1000) --> kreira ponavljajući interval koji se događa svaku sekundu
+    clearInterval(myInterval) --> poništava interval kada nam više ne treba
+*/
+function startCount() {}
